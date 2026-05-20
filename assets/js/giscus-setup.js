f47049febc17222b4523aca4dc77b259fb7a05f2
ahammed-ullah <46980875+ahammed-ullah@users.noreply.github.com>
2026-05-20 04:55:36 +0000

    Deploying to gh-pages from @ ahammed-ullah/ahammed-ullah.github.io@3df2bd2017718401bb70588455b137ec93209f0c 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -9,7 +9,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "ahammed-ullah/homepage_draft",
+    "data-repo": "ahammed-ullah/ahammed-ullah.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
